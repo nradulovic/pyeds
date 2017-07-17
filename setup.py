ed8bf0a506b7e33d42971c042be97cd5f68a88f0
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2017-07-17 20:40:39 +0200

    Fixed list

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='pyeds',
         # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development'
     ],
     keywords='fsm hsm state finite event machine',
