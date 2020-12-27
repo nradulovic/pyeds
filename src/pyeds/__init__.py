c4de1a0269c8358f1bdcc9bdf45bf9d69570e636
Nenad Radulović <nenad.b.radulovic@gmail.com>
2020-12-27 21:38:03 +0100

    Closes #23

--- src/pyeds/__init__.py
+++ src/pyeds/__init__.py
@@ -2,7 +2,7 @@
 Python Event Driven System (PyEDS)
 ==================================
 
-This package provides a means to efficiently write finite state machines (FSM) 
+This package provides a means to efficiently write finite state machines (FSM)
 by hand. The focus was to make the API as simplest as possible since no GUI
 tools are included to define a FSM.
 
